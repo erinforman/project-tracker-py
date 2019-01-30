346a69f3c2b8538622a35970a05c828448e42df8
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-29 18:00:49 -0800

    Remove half done function

--- hackbright.py
+++ hackbright.py
@@ -84,8 +84,7 @@ def get_grade_by_github_title(github, title):
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
-
+pass
 
 def handle_input():
     """Main loop.
